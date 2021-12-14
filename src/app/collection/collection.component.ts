6263ed2d69c26fc763130cef0e8786765fc44f0b
703114283 <Christian.Knoll@universalorlando.com>
2021-12-13 21:06:49 -0500

    getCollectedMovies()

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -23,6 +23,28 @@ export class CollectionComponent implements OnInit {
     this.movieService
       .getMovieList()
       .pipe(
+        map((movieIDs) => {
+          return movieIDs.movies.map((movie: Movie) => {
+            return {
+              id: movie.id,
+              imdbid: movie.imdbid,
+              title: movie.title,
+              posterUrl: movie.posterUrl,
+              watched: movie.watched,
+              liked: movie.liked,
+            };
+          });
+        })
+      )
+      .subscribe((movieData) => {
+        console.log('movieData is', movieData);
+      });
+  }
+
+  getCollectedAndTransformedMovies() {
+    this.movieService
+      .getMovieList()
+      .pipe(
         map((response) =>
           response.movies.map((item: { imdbid: string }) => {
             this.movieService.movieDetails(item.imdbid);
@@ -32,68 +54,5 @@ export class CollectionComponent implements OnInit {
       .subscribe((data) => {
         console.log('data', data);
       });
-
-    // .pipe(
-    //   map((response) =>
-    //     response.movies.map((item: { imdbid: string }) => {
-    //       this.movieService.movieDetails(item.imdbid);
-    //     })
-    //   )
-    // )
-
-    // .pipe(
-    //   map((data) => data.movies),
-    //   map((movie) => {return this.movieService.movieDetails(movie.imdbid)})
-    // )
-
-    // map((movies) =>
-    //     movies.movies
-    //       .map((thisMovie: { imdbid: string }) => {
-    //         return this.movieService.movieDetails(thisMovie.imdbid)
-    //       })
-    //   )
-
-    // getCollectedMovies() {
-    //   this.movieService.getMovieList()
-    //     .pipe(
-    //       map(movieIDs => {
-    //         return movieIDs.movies.map((movie: Movie) => {
-    //           return {
-    //             id: movie.id,
-    //             imdbid: movie.imdbid,
-    //             title: movie.title,
-    //             posterUrl: movie.posterUrl,
-    //             watched: movie.watched,
-    //             liked: movie.liked
-    //           }
-    //         })
-    //       })
-    //     )
-    //     .subscribe(movieData => {
-    //       console.log('movieData is', movieData);
-    //   });
-
-    // console.log('someMovies is', someMovies);
-
-    // pipe(map(movieData => {
-    //   return movieData.movies.map((movie: { _id: string, imdbid: string, watched: boolean, liked: boolean }) => {
-    //     console.log({
-    //       id: movie._id,
-    //       indbid: movie.imdbid,
-    //       watched: movie.watched,
-    //       liked: movie.liked
-    //     });
-    //     return {
-    //       id: movie._id,
-    //       imdbid: movie.imdbid,
-    //       watched: movie.watched,
-    //       liked: movie.liked
-    //     }
-    //   })
-    // }))
-    // .subscribe((transformedMovies) => {
-    //   this.movies = transformedMovies;
-    //   this.moviesUpdated.next([...this.movies]);
-    // })
   }
 }
