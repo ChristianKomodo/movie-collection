92ce9b5d8968de5592f669327f0d643fd95b4c04
Christian <Christian.Knoll@universalorlando.com>
2022-04-18 10:01:43 -0400

    fixed auth

--- server/middleware/check-auth.js
+++ server/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "alone");
+    jwt.verify(token, "there_i_was,_alone_after_dark");
     next();
   } catch (error) {
     res.status(401).json({ message: "Auth failed!" });
