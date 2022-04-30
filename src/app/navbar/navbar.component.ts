6d5156d9f29a0df7270dda59e9b6410ae8869fad
Christian Knoll <chris@sectorfive.com>
2022-04-30 16:08:34 -0400

    login status will persist even if refreshed or they come back later

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,19 +6,33 @@ import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   userIsAuthenticated: boolean = false;
-  private authListenerSubs!: Subscription;  // toDo why is the ! necessary here?  error TS2564
+  private authListenerSubs!: Subscription; // toDo why is the ! necessary here?  error TS2564
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
-      this.userIsAuthenticated = isAuthenticated;
-      console.log('navbar component: userIsAuthenticated is now', this.userIsAuthenticated);
-    });
+    /* 
+      First check to see if user is already authenticated (like if refresh was clicked or 
+      they came back while duration was still less than expiration date) because there is a 
+      chance it was evaluated in App Component before this code runs.
+    */
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    /*
+      Otherwise, subscribe to the authStatusListener to see if user is authenticated
+    */
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+        console.log(
+          'navbar component: userIsAuthenticated is now',
+          this.userIsAuthenticated
+        );
+      });
   }
 
   logOut() {
@@ -28,5 +42,4 @@ export class NavbarComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
-
 }
